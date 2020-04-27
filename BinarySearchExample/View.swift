1790ba2ae999773d9136a87379947fd50ffce923
eonist <30n1st@gmail.com>
2020-04-27 16:24:35 +0200

    Make BinarySearch binaryIdex method support comparable

--- BinarySearchExample/View.swift
+++ BinarySearchExample/View.swift
@@ -3,13 +3,47 @@ import UIKit
 class View: UIView {
    override init(frame: CGRect) {
       super.init(frame: frame)
-      let key = 11
-      var numbers = [9, 15, 91]
-      let idx: Int = BinarySearch.binaryIndex(numbers, i: key, start: 0, end: numbers.count) // 1
+//      test1()
+//      test2()
+//      test3()
+      test4()
+   }
+   /**
+    *
+    */
+   func test1() {
+      let key: Int = 11
+      var numbers: [Int] = [9, 15, 91]
+      let idx: Int = BinarySearch.binaryIndex(numbers, i: key) // 1
       if idx <= numbers.count || numbers[idx] != key { // assert that index is real, and avoid dups
          numbers.insert(key, at: idx)
       }
       print(numbers) // [9, 11, 15, 91]
+   }
+   /**
+    *
+    */
+   func test2() {
+      let numbers: [Int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+      guard let idx: Int = BinarySearch.binarySearch(numbers, key: 43) else { Swift.print("no result"); return }// output: 13 which is the index of where the key is
+      Swift.print("idx:  \(idx)") // 13
+   }
+   /**
+    *
+    */
+   func test3() {
+      let strings: [String] = ["a", "b", "c", "d", "e", "f", "g"]
+      guard let idx: Int = BinarySearch.binarySearch(strings, key: "e") else { Swift.print("no result"); return }
+      Swift.print("idx:  \(idx)") // 4
+   }
+   /**
+    *
+    */
+   func test4() {
+      var strings: [String] = ["a", "b", "d", "e", "g", "j", "m"]
+      let idx: Int = BinarySearch.binaryIndex(strings, i: "f") // 1
+      strings.insert("f", at: idx)
+      print(strings) // ["a", "b", "d", "e", "f", "g", "j", "m"]
    }
    /**
     * Boilerplate
--- README.md
+++ README.md
@@ -7,16 +7,32 @@ BinarySearch methods
 ### Examples:
 
 ```swift
-let key = 11
-var numbers = [9, 15, 91]
-let idx = binaryIndex(numbers, key, 0, numbers.count) // 1
-if idx >= numbers.count || numbers[idx] != key { numbers.insert(key, at: idx) }
+let key: Int = 11
+var numbers: [Int] = [9, 15, 91]
+let idx: Int = BinarySearch.binaryIndex(numbers, i: key) // 1
+if idx <= numbers.count || numbers[idx] != key { // assert that index is real, and avoid dups
+	numbers.insert(key, at: idx)
+}
 print(numbers) // [9, 11, 15, 91]
 ```
 
 ```swift
-let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
-binarySearch(numbers, key: 43, range: 0..< numbers.count) // output: 13 which is the index of where the key is
+let numbers: [Int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+guard let idx: Int = BinarySearch.binarySearch(numbers, key: 43) else { Swift.print("no result"); return }// output: 13 which is the index of where the key is
+Swift.print("idx:  \(idx)") // 13
+```
+
+```swift
+let strings: [String] = ["a", "b", "c", "d", "e", "f", "g"]
+guard let idx: Int = BinarySearch.binarySearch(strings, key: "e") else { Swift.print("no result"); return }
+Swift.print("idx:  \(idx)") // 4
+```
+
+```swift
+var strings: [String] = ["a", "b", "d", "e", "g", "j", "m"]
+let idx: Int = BinarySearch.binaryIndex(strings, i: "f") // 1
+strings.insert("f", at: idx)
+print(strings) // ["a", "b", "d", "e", "f", "g", "j", "m"]
 ```
 
 ### Resources:
